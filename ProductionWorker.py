a289a45dbb2152a873cd56d1cbabee8baeb3ff19
JKapple <jkapple@students.mchenry.edu>
2018-11-13 23:16:57 -0600

    Update ProductionWorker.py

--- ProductionWorker.py
+++ ProductionWorker.py
@@ -1,28 +1,16 @@
-#import Employee
-
-usrFName = input("Enter your first name: ")
-usrLName = input("Enter your last name: ")
-usrShift = input("Enter your shift number: ")
-usrWage = input("Enter your hourly pay rate: ")
+import Employee
 
 
+class ProductionWorker:
+
+    def __init__(self):
+        self.first_name = Employee.usrFName
+        self.last_name = Employee.usrLName
+        self.shift = Employee.usrShift
+        self.wage = Employee.usrWage
 
 
-if usrShift == "1" :
-    usrShift = "Day"
-else:
-    usrShift = "Night"
+print("Your First name is " + Employee.usrFName + "\n Your Last Name is " + Employee.usrFName + "\n Your Shift is " + Employee.usrShift   + "\n Your hourly pay rate is " + Employee.usrWage)
 
-#usrFName,usrLName,usrShift,usrWage
 
-#class ProductionWorker(Employee):
- #   def __init___(self,shift,wage):
-  #      self.shiftNumber = shift
-   #     self.hourlyWage = wage
-    #def get_shift(self):
-     #   return
-#shift = input("Your shift is " + shiftNumber)
-#shift = input("Your shift is " + shiftNumber)
-#return "your shift is" + self.shiftNumber + "/n your wage is "
-
-print ("Your First name is " + usrFName + "\n Your Last Name is " + usrLName  + "\n Your Shift is " + usrShift  + "\n Your hourly pay rate is " + usrWage)
+prd_worker = ProductionWorker()
